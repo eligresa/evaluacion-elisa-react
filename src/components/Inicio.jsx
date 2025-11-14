e42b4137257c8d4ecf921dfbef9ec5003d4fd3d0
eligresa <elisags79@gmail.com>
2025-11-14 13:34:15 +0100

    Añado css

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -6,7 +6,9 @@ export default function Inicio() {
     
     <h1>Super coches de Segunda Mano</h1>
     <p>Esta es una empresa dedicada a la venta de coches de segunda mano</p>
+    <img src="src/coches.jpg" alt="exposición coches" />
     <Link to="/empresas"><button>Ver empresas</button></Link> 
+
     
     </>
   )
